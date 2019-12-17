68da8dec2b23a3983ce6824cff5a5b1945ed4e54
kouno123 <58461563+kouno123@users.noreply.github.com>
2019-12-17 10:31:50 +0900

    Add files via upload

---
+++ CoreBot1/Dialogs/IDialogContext.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+//
+// Generated with Bot Builder V4 SDK Template for Visual Studio CoreBot v4.6.2
+
+namespace CoreBot1.Dialogs
+{
+    public interface IDialogContext
+    {
+    }
+}
