1dd76421386e3415ba8f9a11595f8710afd2ee37
kouno123 <58461563+kouno123@users.noreply.github.com>
2019-12-17 10:27:28 +0900

    Add files via upload

---
+++ CoreBot1/Rido.AuthProxy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace Rido.AuthProxy
+{
+    public class AuthProxyModule : IWebProxy
+    {
+        ICredentials crendential = new NetworkCredential("proxy.user", "password");
+        public ICredentials Credentials
+        {
+            get
+            {
+                return crendential;
+            }
+            set
+            {
+                crendential = value;
+            }
+        }
+        public Uri GetProxy(Uri destination)
+        {
+            return new Uri("http://proxy:8080", UriKind.Absolute);
+        }
+        public bool IsBypassed(Uri host)
+        {
+            return host.IsLoopback;
+        }
+    }
+}
